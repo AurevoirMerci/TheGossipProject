3e0889c81469ddbbf38d4db7af447d75f86490ec
AdrienPeligry <adrien.peligry@essec.edu>
2019-01-31 23:50:50 +0100

    Hodor? Hodor.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do
+    tag = Tag.create(title: Faker::TvShows::BreakingBad.character)
+end
+
+10.times do
+    city = City.create(name: Faker::WorldCup.city, zip_code: Faker::IDNumber.valid)
+end
+
+10.times do
+    user = User.create(first_name: Faker::Team.mascot, last_name: Faker::Team.state, description: Faker::Space.agency, email: Faker::Company.australian_business_number, age: rand(10...42), city_id: rand(1..10))
+end
+
+20.times do
+    gossip = Gossip.create(title: Faker::Esport.team, content: Faker::Esport.player, user_id: rand(1..10))
+end
+
+10.times do 
+	join_gossip_tag = JoinGossipTag.create(gossip_id: rand(1..20), tag_id: rand(1..10))
+end
+
+
+15.times do 
+	private_message = PrivateMessage.create(content: Faker::TvShows::GameOfThrones.quote, sender_id: rand(1..10), recipient_id: rand(1..10))
+end
+
+
